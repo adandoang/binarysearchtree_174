10797ff2f60db366ae455bc9925b072136185f46
adandoang <akhdanjzif@gmail.com>
2023-07-02 17:46:51 +0700

    if

--- binarysearchtree_174/binarysearchtree_174.cpp
+++ binarysearchtree_174/binarysearchtree_174.cpp
@@ -92,5 +92,11 @@ public:
 			cout << "tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
 	}
 };
