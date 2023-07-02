7ad1d698eed25ef7d3f1db970d5ea405473f7c03
adandoang <akhdanjzif@gmail.com>
2023-07-02 15:11:47 +0700

    void search

--- binarysearchtree_174/binarysearchtree_174.cpp
+++ binarysearchtree_174/binarysearchtree_174.cpp
@@ -54,4 +54,12 @@ public:
 			parent->rightchild = newNode; // make the right of the parent point to the new node
 		}
 	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		//this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+	}
 };
